7bc7ef59b501c1ea5a1eb78bce0d7ca64edc895a
RH-students <rh-students@feedhenrys-MacBook-Pro.local>
2018-02-26 11:32:00 +0000

    Update

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,14 @@
 var inputScreen = document.getElementById("screenInput");
+var equation = "";
 
-function addNumberToInput(number){
+function addNumberToInput(number) {
     var input = inputScreen.value;
     inputScreen.value = input + number;
+}
+function addOperatorToInput(operator) {
+    var input = inputScreen.value;
+    equation += inputScreen.value + operator;
+}
+function clearInput(clear) {
+    inputScreen.value = ""
 }
